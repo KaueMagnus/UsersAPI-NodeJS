98965375052f5f7279ae72bed80aaa7249a1dfdc
KaueMagnus <kmarquesmagnus@gmail.com>
2025-06-30 18:16:33 -0300

    add PUT and DELETE routes; query params

--- server.js
+++ server.js
@@ -27,19 +27,54 @@ app.post('/users', async (req, res) => {
 // Get all users
 app.get('/users', async (req, res) => {
 
-    const findUsers = await prisma.user.findMany()
+    let users = []
 
-    res.status(200).json(findUsers)
+    if (req.query) {
+        users = await prisma.user.findMany({
+            where: {
+                name: req.query.name,
+                age: req.query.age,
+                email: req.query.email
+                
+            }
+        })
+    } else {
+        users = await prisma.user.findMany()
+    }
+
+    res.status(200).json(users)
 })
 
 // Edit user
-// app.put("/users")
+app.put('/users/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
 
 // Edit a specific attribute of the user
 //app.patch("/users")
 
 // Delete user
-// app.delete("/users")
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: "User deleted successfully!"})
+})
 
 
 app.listen(3000)
